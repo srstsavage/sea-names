d69ad8b47ee5530cd82e0d64c9628358febea81d
Luke Campbell <luke@axiomdatascience.com>
2024-06-21 12:45:09 -0400

    Version 0.2.0 -- Sea names by trajectory

--- sea_names/__init__.py
+++ sea_names/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Luke Campbell"""
 __email__ = "luke@axds.co"
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 from .geo import (
     get_region_polygons,
--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='http://git.axiom/axiom/sea-names',
-    version='0.1.2',
+    version='0.2.0',
     zip_safe=False,
 )
